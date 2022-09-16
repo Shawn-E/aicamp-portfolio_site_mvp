d396dc27baed6c4eea631031053e3df2f243b26d
Shawn Engmann <86177860+Shawn-E@users.noreply.github.com>
2022-09-16 11:56:20 -0400

    Commit 1 - 9/16 | Added New Pages + Routing, Added Form Submit, Added From Blacklist

--- index.js
+++ index.js
@@ -3,21 +3,14 @@ const express = require('express');
 const cfg = require('./config.json')
 //initialize express app
 const app = express();
-
 app.use(express.static('public'));
-
 app.use('/assets', express.static(__dirname + 'public/assets'))
-
 app.set('views', './src/views');
-
 app.set('view engine', 'ejs');
-
 // run the routing file
 require("./src/routing.js")(app, cfg)
-
 app.listen(cfg.port)
 console.log('MVP Started - Port: ' + cfg.port);
-
 // Rejection Handler
 process.on('unhandledRejection', (err) => { 
     console.log(err);
--- src/routing.js
+++ src/routing.js
@@ -1,8 +1,47 @@
 module.exports = async function(app, cfg) {
-
-	app.get("/", async function(req, res){
+	// Index Page
+	app.get('/', async function(req, res){
 		res.render('index', {
 		});
 	});
+
+	// Skills Page
+	app.get('/skills', async function(req, res){
+		res.render('skills', {
+		});
+	});
+
+	// Contact Page
+	app.get('/contact', async function(req, res){
+		res.render('contact', {
+
+		})
+	})
+
+	// Contact Form 
+	app.get('/backend/forms/submit/contact', async function(req, res){
+		let name = req.body.name.replaceAll("'", "''").replaceAll('"', '""').replaceAll('`', '``');
+		let email = req.body.email.replaceAll("'", "''").replaceAll('"', '""').replaceAll('`', '``');
+		let subject = req.body.subject.replaceAll("'", "''").replaceAll('"', '""').replaceAll('`', '``');
+		let message = req.body.message.replaceAll("'", "''").replaceAll('"', '""').replaceAll('`', '``');
+
+		// Blacklist System
+		let blacklisted_names = ["CryptoCrab"]
+		let blacklisted_emails = ["s@shawnengmann.com"]
+		let blacklisted_subjects = ["Free Money"]
+		let blacklisted_message = ["Free Products"]
+
+		// Checking if requests contains blacklisted items
+
+		if(blacklisted_names.contains(name) || blacklisted_emails.contains(email) || blacklisted_subjects.contains(subject) || blacklisted_message.contains(message)){ 
+			res.send("From Entry Denied")
+		} else {
+			res.redirect(`/contact/?submitted=${encodeURIComponent('true')}`);
+		}
+
+
+
+	});
 }
+
 
